785263de4b626e914a901454406d8e4d75bc5d64
d3npä <d3npa@protonmail.com>
2019-01-01 16:37:48 -0500

    Fix links for github.io

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ function changeLanguage() {
     if (currentLanguage == "en") {
         document.getElementById("lang_toggle").innerHTML = "<i class=\"fa fa-globe-americas\"></i> View English version";
         document.getElementById("lang_toggle").setAttribute("lang", "ja");
-        document.getElementById("lang_control").setAttribute("href", "/css/lang_ja.css");
+        document.getElementById("lang_control").setAttribute("href", "css/lang_ja.css");
     }
     else {
         document.getElementById("lang_toggle").innerHTML = "<i class=\"fa fa-globe-asia\"></i> 日本語版を見る";
         document.getElementById("lang_toggle").setAttribute("lang", "en");
-        document.getElementById("lang_control").setAttribute("href", "/css/lang_en.css");
+        document.getElementById("lang_control").setAttribute("href", "css/lang_en.css");
     }
 }
 
