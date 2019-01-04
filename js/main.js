e1d0de360d7910962ef3c50022430cd3d32c5856
d3npä <d3npa@protonmail.com>
2019-01-04 16:01:37 -0500

    Handlebars.jsを追加して元index.htmlをベース・テンプレートにした

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 function changeLanguage() {
     var currentLanguage = document.getElementById("lang_toggle").getAttribute("lang");
     if (currentLanguage == "en") {
-        document.getElementById("lang_toggle").innerHTML = "<i class=\"fa fa-globe-americas\"></i> View English version";
         document.getElementById("lang_toggle").setAttribute("lang", "ja");
         document.styleSheets[0].disabled = false;
         document.styleSheets[1].disabled = true;
     }
     else {
-        document.getElementById("lang_toggle").innerHTML = "<i class=\"fa fa-globe-asia\"></i> 日本語版を見る";
         document.getElementById("lang_toggle").setAttribute("lang", "en");
         document.styleSheets[0].disabled = true;
         document.styleSheets[1].disabled = false;
