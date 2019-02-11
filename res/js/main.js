f0c5e261addc31c48d9c07da8808746f0dac4d0b
himitu23 <himitu23centcircumflex@gmail.com>
2019-02-11 18:37:26 +0900

    [update] some pages added

--- res/js/main.js
+++ res/js/main.js
@@ -11,3 +11,19 @@ function changeLanguage() {
         document.styleSheets[1].disabled = false;
     }
 }
+
+//add for get parmeter when page transition occured
+function getParam()
+{
+    var url   = location.href
+    parameters    = url.split("?")
+    params   = parameters[1].split("&")
+    var paramsArray = []
+    for ( it = 0; it < params.length; it++ ) {
+        neet = params[it].split("=")
+        paramsArray.push(neet[0])
+        paramsArray[neet[0]] = neet[1]
+        }
+    var categoryKey = paramsArray["id"]
+    return categoryKey
+}
